872f13e7f3b3eea2d0c6035fe2fd013b50548c3b
Williams <23678bella23678@gmail.com>
2024-04-23 13:44:20 -0400

    Initial MyMiniCourse.js redo

---
+++ MyMiniCourse.js
@@ -0,0 +1,10 @@
+// Programmer: Bella
+// Team Member: Hailioso
+// Bolt ID: B2F8
+// Date: 4.23.2024
+// Program: My Mini Course
+// Program URL: https://edu.sphero.com/program/16743344/edit
+
+async function startProgram() {
+	// Write code here
+}
