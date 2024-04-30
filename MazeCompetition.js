e232bc1aae5753313b72b8b5af6553535e5bff70
Williams <23678bella23678@gmail.com>
2024-04-30 13:28:26 -0400

    Added Beginning Comments and start program Java Script

---
+++ MazeCompetition.js
@@ -0,0 +1,11 @@
+// Programmer: Bella
+// Team Member: Hailioso
+// Bolt ID: B2F8
+// Date: 4.30.2024
+// Program: Maze Competition
+// Program URL: https://edu.sphero.com/program/16788331/edit
+
+
+async function startProgram() {
+	// Write code here
+}
