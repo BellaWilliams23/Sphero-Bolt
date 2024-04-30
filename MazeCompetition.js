10dfcef2995f2c28495d0c5fefcfa2aa0ccb84b6
Williams <23678bella23678@gmail.com>
2024-04-30 14:16:29 -0400

    // Programmer: Bella Williams - Start Text & Audio Checkpoint Commit - Date: 4.30.24

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -5,7 +5,39 @@
 // Program: Maze Competition
 // Program URL: https://edu.sphero.com/program/16788331/edit
 
-
 async function startProgram() {
-	// Write code here
+
+	// Programmer: Bella Williams - Start Text & Audio Checkpoint Commit - Date: 4.30.24
+	await speak("START", true)
+	await scrollMatrixText('START', { r:66, g: 56, b: 255 }, 20 , true) 
+
+
+	// Programmer:            - Blue LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:            - Sound One Checkpoint Commit - Date:
+
+
+
+	// Programmer:            - Red LED Checkpoint Commit - Date: 
+
+
+
+	// Programmer:            - Sound Two Checkpoint Commit - Date:
+
+
+
+	// Programmer:            - Green LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:            - Purple LED & Sound Three Checkpoint Commit - Date:
+
+
+
+	// Programmer:            - Finish Text & Audio Checkpoint Commmit - Date:
+
+
+
 }
