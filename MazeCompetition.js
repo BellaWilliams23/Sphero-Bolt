dbb0aa932ef393b992d9bd48f16ec33cafad5dd9
Williams <23678bella23678@gmail.com>
2024-05-01 14:20:02 -0400

    // Programmer: Hailey Terrini - Blue LED Checkpoint Commit - Date: 5.1.24

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -1,9 +1,12 @@
 // Programmer: Bella
 // Team Member: Hailioso
 // Bolt ID: B2F8
-// Date: 4.30.2024
-// Program: Maze Competition
-// Program URL: https://edu.sphero.com/program/16788331/edit
+// Date: 5.1.2024
+// Program: Obstacle Course
+// Program URL: https://edu.sphero.com/program/16752511/editasync function startProgram() {
+	
+
+
 
 async function startProgram() {
 
@@ -11,33 +14,34 @@ async function startProgram() {
 	await speak("START", true)
 	await scrollMatrixText('START', { r:66, g: 56, b: 255 }, 20 , true) 
 
-
-	// Programmer:            - Blue LED Checkpoint Commit - Date:
-
-
-
-	// Programmer:            - Sound One Checkpoint Commit - Date:
-
-
-
-	// Programmer:            - Red LED Checkpoint Commit - Date: 
+	
+	
+	// Programmer: Hailey Terrini - Blue LED Checkpoint Commit - Date: 5.1.24
+	setMainLed({ r: 0, g: 0, b: 255});
+	await roll((getHeading() -180), 70, 2.5);
 
 
 
-	// Programmer:            - Sound Two Checkpoint Commit - Date:
+	// Programmer          - Sound One Checkpoint Commit - Date:
 
 
 
-	// Programmer:            - Green LED Checkpoint Commit - Date:
+	// Programmer:        - Red LED Checkpoint Commit - Date:
 
 
 
-	// Programmer:            - Purple LED & Sound Three Checkpoint Commit - Date:
+	// Programmer          - Sound Two Checkpoint Commit - Date:
 
 
 
-	// Programmer:            - Finish Text & Audio Checkpoint Commmit - Date:
+	// Programmer:        - Green LED Checkpoint Commit - Date:
 
 
+
+
+	// Programmer:        - Purple LED & Sound Three Checkpoint Commit - Date:
+
+
+	// Programmer:        - Finish Text & Audio Checkpoint Commit - Date:
 
 }
